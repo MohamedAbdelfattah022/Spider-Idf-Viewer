14dc05b67a736938bb177ab73d549bbc896c9b1a
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2025-03-03 18:32:14 +0200

    refactor styles for improved readability and modern design

--- spider-idf-viewer.js
+++ spider-idf-viewer.js
@@ -2,7 +2,7 @@ const source = "https://github.com/ladybug-tools/spider-2020/tree/master/spider-
 
 const version = "v-2020-10-09";
 
-const description = document.head.querySelector( "[ name=description ]" ).content;
+const description = document.head.querySelector("[ name=description ]").content;
 
 const path = "https://www.ladybug.tools/spider-2020/spider-idf-viewer/idf-sample-files/";
 
@@ -28,7 +28,7 @@ const files = [
 	//"test-case-2020-06-13.zip",
 ];
 
-const urlDefault = "https://www.ladybug.tools/spider-2020/spider-idf-viewer/idf-sample-files/theoretical-office-building-tel-aviv-2020-06-15.zip"
+// const urlDefault = "https://www.ladybug.tools/spider-2020/spider-idf-viewer/idf-sample-files/theoretical-office-building-tel-aviv-2020-06-15.zip"
 //const urlDefault = path + files[ 2 ];
 
 
@@ -80,9 +80,9 @@ function init() {
 	JTH.init();
 
 
-	if ( ! location.hash ) {
+	if (!location.hash) {
 
-		FOO.requestFile( urlDefault );
+		FOO.requestFile(urlDefault);
 
 	}
 
@@ -113,7 +113,7 @@ FOO.callback2 = () => {
 
 	THR.campus.add(...surfaces.lines, ...surfaces.meshes);
 
-	ItoJ.checkZones( THR.campus);
+	ItoJ.checkZones(THR.campus);
 
 	THR.campus.position.x = count * 50 - 100;
 
@@ -152,19 +152,19 @@ function openFile(index = 0) {
 const ItoJ = {};
 
 
-ItoJ.init = function() {
+ItoJ.init = function () {
 
-	ItoJ.strings = ItoJ.split( FOO.string ).map( item => item.split(/\n/g) );
+	ItoJ.strings = ItoJ.split(FOO.string).map(item => item.split(/\n/g));
 	//console.log( "strings", ItoJ.strings.slice( 0, 15 ) );
 
-	ItoJ.parents = ItoJ.getParents( ItoJ.strings );
+	ItoJ.parents = ItoJ.getParents(ItoJ.strings);
 	//console.log( "ItoJ.parents", ItoJ.parents );
 
 
-	htm = ItoJ.parents.keys.map( ( key, index ) => `
-	<details ontoggle=ItoJ.parseValues(${ index })><summary>${ key }</summary>
-		<div id=ItoJdiv${ index } ></div>
-	</details>` ).join( "");
+	htm = ItoJ.parents.keys.map((key, index) => `
+	<details ontoggle=ItoJ.parseValues(${index})><summary>${key}</summary>
+		<div id=ItoJdiv${index} ></div>
+	</details>` ).join("");
 
 	JTVdivJsonTree.innerHTML = "<p>JSON Tree View</p>" + htm;
 
@@ -173,54 +173,54 @@ ItoJ.init = function() {
 };
 
 
-ItoJ.checkZones = function( group = THR.group ) {
+ItoJ.checkZones = function (group = THR.group) {
 
-	const index = ItoJ.parents.keys.indexOf( "  Zone" )
+	const index = ItoJ.parents.keys.indexOf("  Zone")
 	//console.log( "index", index,  ItoJ.parents.values[ index + 1]);
 
-	const zonesArr = ItoJ.parents.values[ index + 1].map( item => item[ 1 ])
+	const zonesArr = ItoJ.parents.values[index + 1].map(item => item[1])
 	//console.log( "zonesArr", zonesArr );
 
-	if ( zonesArr.length < 2 ) { return; }
+	if (zonesArr.length < 2) { return; }
 
-	const names = zonesArr.map( zone => zone[ 1 ].split( "," ).shift().trim() );
+	const names = zonesArr.map(zone => zone[1].split(",").shift().trim());
 	//console.log( "names",  names);
 
-	const deltaArrays = zonesArr.map( zone => zone.slice( 3, 6 ).map( item => + item.trim().split( "," ).shift() ));
+	const deltaArrays = zonesArr.map(zone => zone.slice(3, 6).map(item => + item.trim().split(",").shift()));
 
-	const deltas = deltaArrays.map( delta => new THREE.Vector3().fromArray(  delta ) );
+	const deltas = deltaArrays.map(delta => new THREE.Vector3().fromArray(delta));
 	//console.log( "deltas", deltas );
 
-	for ( let i = 0; i < deltas.length; i++ ) {
+	for (let i = 0; i < deltas.length; i++) {
 
-		const delta = deltas[ i ];
+		const delta = deltas[i];
 		//console.log( "len", delta.length() );
 
-		if ( delta.length() === 0 ) { continue }
+		if (delta.length() === 0) { continue }
 
-		const name = names[ i ];
+		const name = names[i];
 
-		for ( let child of group.children ) {
+		for (let child of group.children) {
 
 			let childName = "";
 
-			if ( child.userData.text && child.userData.text.includes( name + "," ) ) {
+			if (child.userData.text && child.userData.text.includes(name + ",")) {
 
-				child.position.add( delta );
+				child.position.add(delta);
 
-				childName = child.userData.text.split(/\n/g)[ 1 ].trim().split( ",")[ 0 ];
+				childName = child.userData.text.split(/\n/g)[1].trim().split(",")[0];
 				//console.log( "name", childName );
 			}
 
-			if ( !childName ) { continue; }
+			if (!childName) { continue; }
 
-			for ( let child2 of group.children ) {
+			for (let child2 of group.children) {
 
-				if ( ["door", "glassdoor", "window"].includes( child2.userData.type) &&
-					child2.userData.text.includes( childName ) && !child2.moved ) {
+				if (["door", "glassdoor", "window"].includes(child2.userData.type) &&
+					child2.userData.text.includes(childName) && !child2.moved) {
 
 					//console.log( "item",  child );
-					child2.position.add( delta );
+					child2.position.add(delta);
 
 					child2.moved = true;
 
@@ -234,55 +234,55 @@ ItoJ.checkZones = function( group = THR.group ) {
 
 	//if ( !zonesArr[ 0 ][ 7 ] )  { return; }
 
-	if ( !zonesArr[ 0 ].find( line => line.includes( "Multiplier" ) ) ) {
-		console.log( "no multiplier found" );
+	if (!zonesArr[0].find(line => line.includes("Multiplier"))) {
+		console.log("no multiplier found");
 		return;
 	}
 
 
-	const multiplierArray = zonesArr.map( zone => {
+	const multiplierArray = zonesArr.map(zone => {
 
-		zoneLine = zone.find( line => line.includes( "Multiplier" ) );
+		zoneLine = zone.find(line => line.includes("Multiplier"));
 
-		return  zoneLine.trim().split( "," ).shift();
+		return zoneLine.trim().split(",").shift();
 
-	} );
+	});
 
 	//console.log( "multiplierArray", multiplierArray );
 
-	for ( let i = 0; i < multiplierArray.length; i++ ) {
+	for (let i = 0; i < multiplierArray.length; i++) {
 
-		const multiplier = + multiplierArray[ i ];
+		const multiplier = + multiplierArray[i];
 
-		if ( multiplier < 2 ) {  }
+		if (multiplier < 2) { }
 
-		zoneName = zonesArr[ i ][ 1 ].split( "," ).shift().trim();
+		zoneName = zonesArr[i][1].split(",").shift().trim();
 		//console.log( "zoneName", zoneName );
 
 		const zoneGroup = new THREE.Group();
 
-		for ( let child of group.children ) {
+		for (let child of group.children) {
 
-			if ( child.userData.zone && child.userData.zone === zoneName ) {
+			if (child.userData.zone && child.userData.zone === zoneName) {
 
 				//console.log( "child", i, child.userData.zone );
 
 				clone = child.clone();
-				zoneGroup.add( clone );
+				zoneGroup.add(clone);
 
 			}
 
 
 		}
 
-		const height= new THREE.Box3().setFromObject( zoneGroup ).getSize( new THREE.Vector3() ).z;
+		const height = new THREE.Box3().setFromObject(zoneGroup).getSize(new THREE.Vector3()).z;
 		//console.log( "height", height );
 
-		for ( let j = 0; j < multiplier; j++ ) {
+		for (let j = 0; j < multiplier; j++) {
 
 			const group = zoneGroup.clone();
 			group.position.z = j * height;
-			THR.group.add( group );
+			THR.group.add(group);
 		}
 
 
@@ -291,42 +291,42 @@ ItoJ.checkZones = function( group = THR.group ) {
 };
 
 
-ItoJ.parseValues = function( index ) {
+ItoJ.parseValues = function (index) {
 
-	div = JTVdivJsonTree.querySelector( "#ItoJdiv" + index );
+	div = JTVdivJsonTree.querySelector("#ItoJdiv" + index);
 
 	//console.log( "value", ItoJ.parents.values[ index + 1].slice( 1 ) )
 
-	let arrs = ItoJ.parents.values[ index + 1];
+	let arrs = ItoJ.parents.values[index + 1];
 
 	let htm = "";
 
-	if ( arrs.length === 1 ) {
+	if (arrs.length === 1) {
 
-		const arr = arrs[ 0 ].slice( 1 )[ 0 ].slice( 1 )
-		const items = arr.map( item => item.split( "!- " ) )
+		const arr = arrs[0].slice(1)[0].slice(1)
+		const items = arr.map(item => item.split("!- "))
 
-		htm = items.map( item => `<div><small><b>${ item[ 1 ] }:</b></small> <span class=value >${ item[ 0 ].trim().replace( /[,;]/, "") }</span></div>` ).join( "");
+		htm = items.map(item => `<div><small><b>${item[1]}:</b></small> <span class=value >${item[0].trim().replace(/[,;]/, "")}</span></div>`).join("");
 
 	} else {
 
 		//console.log( "arrs", arrs );
 
-		htm = arrs.map( arr => {
+		htm = arrs.map(arr => {
 
-			arr = arr.slice( 1 )[ 0 ].slice( 1 );
+			arr = arr.slice(1)[0].slice(1);
 			//console.log( "arr", arr );
 
-			const items = arr.map( item => item.split( "!- " ) )
+			const items = arr.map(item => item.split("!- "))
 			//console.log( "item", items );
 
-			const htm = items.map( item =>
-				`<div><small><b>${ item[ 1 ] }:</b></small> <span class=value >${ item[ 0 ].trim().replace( /[,;]/, "")}</span></div>` );
+			const htm = items.map(item =>
+				`<div><small><b>${item[1]}:</b></small> <span class=value >${item[0].trim().replace(/[,;]/, "")}</span></div>`);
 			//console.log( "htm", htm.join( "") );
 
-			return htm.join( "") ;
+			return htm.join("");
 
-		} ).join( "<hr>");
+		}).join("<hr>");
 
 		//console.log( "htm", htm);
 
@@ -337,34 +337,34 @@ ItoJ.parseValues = function( index ) {
 }
 
 
-ItoJ.getParents = function( items ) {
+ItoJ.getParents = function (items) {
 
 	const keys = [];
 	const values = [];
 	let value = ["undefined"];
 
 
-	for ( let i = 0; i < items.length; i++ ) {
+	for (let i = 0; i < items.length; i++) {
 
-		const item = items[ i ];
+		const item = items[i];
 
-		let key = item[ 0 ];
+		let key = item[0];
 
-		key = key.length === 1 ? "Version" : key.replace( ",", "");
+		key = key.length === 1 ? "Version" : key.replace(",", "");
 
-		if ( key.startsWith( "!" ) ) { continue; }
+		if (key.startsWith("!")) { continue; }
 
-		if ( ! keys.includes( key )){
+		if (!keys.includes(key)) {
 
-			keys.push( key );
+			keys.push(key);
 
-			values.push( value );
+			values.push(value);
 
-			value = [ [key, item ] ];
+			value = [[key, item]];
 
 		} else {
 
-			value.push( [ key, item ] )
+			value.push([key, item])
 		}
 	}
 
@@ -373,11 +373,11 @@ ItoJ.getParents = function( items ) {
 };
 
 
-ItoJ.split = function( string ) {
+ItoJ.split = function (string) {
 
 	let arr = string.split(/\n\n/g);
 
-	if ( arr.length < 3 ) {
+	if (arr.length < 3) {
 
 		arr = string.split(/\r\n\r\n/g);
 
@@ -399,7 +399,7 @@ RAY.getHtm = function (intersected) {
 	const obj = RAY.intersected.object;
 	const index = obj.userData.index;
 	const mesh = THR.group.children[index];
-	console.log( "obj", obj );
+	console.log("obj", obj);
 	const htm = `
 	<div>
 		file: ${obj.userData.fileName}<br>
